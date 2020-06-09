a80877189c4541ab93f1d14b48863baa8861aa47
Dmitry Petrov <e.liberate@gmail.com>
2020-06-09 02:41:35 +0000

    Added a form for a new URL

--- express_server.js
+++ express_server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 3000; // default port 8080
 
 app.set("view engine", "ejs")
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+
 };
 
 app.get("/", (req, res) => {
@@ -18,8 +25,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req);
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
